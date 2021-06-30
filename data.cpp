37abddfe48439051e168cb443ba66589a590e22f
Marco Chitolina <68135699+MarcoChit0@users.noreply.github.com>
2021-06-30 18:11:47 -0300

    removing debug lines

--- data.cpp
+++ data.cpp
@@ -257,50 +257,44 @@ Set algorithm_for_NEMO(vector <Arc *> arcs, Set trails, int capacity){
     // c <- 0
     int cover_index = 0; 
     // CoveredBy(a) <- null, for each a in A
-    for(int i=0; i<unassigned_flows.size(); i++){
-        for(int j=0; j<unassigned_flows.getTrailSize(i); j++){
-            unassigned_flows.changeCover(NO_COVER, j, i); 
-        }
-    }
+        // coveredBy = -1 for all arcs as default 
     // UF <- sort
         // sorted in ascending order by the length of each trail and,
         // in case of ties, in descending order by the sum of the number of trails covering ther arcs of each trail
     unassigned_flows.sort(); 
-    unassigned_flows.print();
-    system("pause");
     // for each f in UF do... 
-    for(int flow=0; flow< unassigned_flows.size(); flow++){ cout << "flow : " << flow << endl; // debug
+    for(int flow=0; flow< unassigned_flows.size(); flow++){ 
         // h <- 0
         unassigned_flows.changeTrailHead(flow, 0);
         // t <- 0 
         unassigned_flows.changeTrailTail(flow, 0);
             // head and tail for each tail in unassigned_flows are already equal to EMPTY_TRAIL = 0
         // while t < |f| do... 
-        while(unassigned_flows.getTrailTail(flow) < unassigned_flows.getTrailSize(flow)){ cout << "1" <<endl; // debug
+        while(unassigned_flows.getTrailTail(flow) < unassigned_flows.getTrailSize(flow)){ 
             // g <- coveredBy(f(t))
             int aux = unassigned_flows.coveredBy(flow, unassigned_flows.getTrailTail(flow));
             // if g = null, then...
-            if (aux == NO_COVER){cout << "2" <<endl; // debug
+            if (aux == NO_COVER){
                 // t <- t + 1
                 unassigned_flows.changeTrailTail(flow, (unassigned_flows.getTrailTail(flow) + 1));
             }
             // else...
-            else{cout << "3" <<endl; // debug
+            else{
                 // if subtrail(S(g),f), then... 
-                if(isSubtrail(sol.getTrailArcs(aux), unassigned_flows.getTrailArcs(flow))){cout << "4" <<endl; // debug
+                if(isSubtrail(sol.getTrailArcs(aux), unassigned_flows.getTrailArcs(flow))){
                     // t <- t + |S(g)|
                     unassigned_flows.changeTrailTail(flow, (sol.getTrailArcs(aux).size() + unassigned_flows.getTrailTail(flow) -1)); 
                     // S(g) <- null
                     sol.deleteTrail(aux); 
                 }
                 // else...
-                else{cout << "5" <<endl; // debug
+                else{
                     // if |f(h:t)| > 0, then... 
-                    if(unassigned_flows.subtrailSize(flow)){ cout << "6" <<endl; // debug
+                    if(unassigned_flows.subtrailSize(flow)){
                         // S <- S u f(h:t) 
                         sol.addTrail(unassigned_flows.getTrail(flow)); 
                         // for each a in f(h:t) do... 
-                        for(int i= unassigned_flows.getTrailHead(flow); i < unassigned_flows.getTrailTail(flow); i++) {cout << "7" <<endl; // debug
+                        for(int i= unassigned_flows.getTrailHead(flow); i < unassigned_flows.getTrailTail(flow); i++) {
                             // CoveredBy(a) <- c
                             sol.changeCover(cover_index, i, flow); 
                         }
@@ -310,7 +304,7 @@ Set algorithm_for_NEMO(vector <Arc *> arcs, Set trails, int capacity){
                         unassigned_flows.changeTrailHead(flow, unassigned_flows.getTrailTail(flow)); 
                     }
                     // else... 
-                    else{ cout << "8" <<endl; // debug 
+                    else{ 
                         // h <- h + 1
                         unassigned_flows.changeTrailHead(flow, unassigned_flows.getTrailHead(flow) + 1);
                         // t <- h
@@ -321,17 +315,13 @@ Set algorithm_for_NEMO(vector <Arc *> arcs, Set trails, int capacity){
         }
 
         // if |f(h:t)| > 0, then... 
-        if(unassigned_flows.subtrailSize(flow)){cout << "9" <<endl; // debug
+        if(unassigned_flows.subtrailSize(flow)){
             // S <- S u f(h:t) 
             sol.addTrail(unassigned_flows.getTrail(flow));
-            sol.print();
-            system("pause");
             // for each a in f(h:t) do... 
-            for(int i=0; i < (unassigned_flows.getTrailTail(flow) - unassigned_flows.getTrailHead(flow ) ); i++) {cout << "10" <<endl; // debug
+            for(int i=0; i < (unassigned_flows.getTrailTail(flow) - unassigned_flows.getTrailHead(flow ) ); i++) {
                 sol.changeCover(cover_index, i, flow); 
             }
-            sol.print();
-            system("pause");
             // c <- c + 1
             cover_index = cover_index + 1; 
         }
